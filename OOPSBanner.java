66d3fab9f5bd095f97c6fc3a6ef653dd1112216d
ag1193-pixel <ag1193@srmist.edu.in>
2026-03-03 07:17:26 +0000

    created OOPSBanner

--- OOPSBanner.java
+++ OOPSBanner.java
@@ -1,13 +1,51 @@
 public class OOPSBanner {
-    public static void main(String[] args) {
+    
+    public static String[] getOPattern(){
+        return new String[]{"   ***   ",
+                            " **   ** ",
+                            "**     **",
+                            "**     **",
+                            "**     **",
+                            " **   ** ",
+                            "   ***   "
 
-        System.out.println(String.join(" ", "   *   ", "   *    ", " *****  "  ,"   ***  "));
-        System.out.println(String.join(" ", " *   * ", " *   *  ", " *    *  ", " **    ** "));
-        System.out.println(String.join(" ", "*     *", "*     * ", " *****  " , "   **     "));
-        System.out.println(String.join(" ", "*     *", "*     * ", " *       ", "     ***   "));
-        System.out.println(String.join(" ", "*     *", "*     * ", " *       ", "       **  "));
-        System.out.println(String.join(" ", " *   * ", " *   *  ", " *       ", " **    **  "));
-        System.out.println(String.join(" ", "   *   ", "   *    ", " *       ", "   ***    "));
+        };
+    }
+    public static String[] getPPattern(){
+        return new String[] {" ******  ",
+                             " *    *  ",
+                             " ******  ",
+                             " *       ",
+                             " *       ",
+                             " *       ",
+                             " *       "
 
+        };
+    }
+    public static String[] getSPattern(){
+        return new String[] {"   ***   ",
+                             " **      ",
+                             "  **     ",
+                             "    ***  ",
+                             "       **",
+                             " **    **",
+                             "   ***   "
+
+        };
+    }
+  
+    public static void main(String[] args){
+        String[] oPattern = getOPattern();
+        String[] pPattern = getPPattern();
+        String[] sPattern = getSPattern();
+        
+        for (int i = 0; i < oPattern.length;i++){
+            System.out.println(
+                oPattern[i]+" "+
+                oPattern[i]+" "+
+                pPattern[i]+" "+
+                sPattern[i]
+            );
+        }
     }
 }
